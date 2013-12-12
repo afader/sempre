e6c6cac8919511b4e1821fe0454b0ff1a2a7595e
Anthony Fader <afader@gmail.com>
2013-12-11 17:14:21 -0800

    updated to include derivation

--- parse_output.py
+++ parse_output.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
 import re
 import sys
 data = dict()
 qpat = re.compile(r'Example: (.*) \{')
 ppat = re.compile(r'"(.*?)"')
 spat = re.compile(r'score=([-0-9.]+)')
+dpat = re.compile(r'\(derivation \((.*)\) \[')
 question = None
 for line in sys.stdin:
     line = line.strip()
@@ -12,5 +15,6 @@ for line in sys.stdin:
     if line.startswith('Pred@0000:'):
         answers = ppat.findall(line)
         score = float(spat.findall(line)[0])
+        deriv = dpat.findall(line)[0]
         for a in answers:
-            print '%s\t%s\t%s' % (question, a, score)
+            print '%s\t%s\t%s\t%s' % (question, a, score, deriv)
